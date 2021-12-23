b85a9952479a8132d11394345676b294fb732ba9
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-12-23 21:28:59 +0100

    first commit

---
+++ src/components/Singer.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="singer">
+		<h1 class="singer__title">Hello from {{value}} !</h1>
+	</div>
+</template>
+
+<script>
+/*
+	Entry point for vue components
+*/
+
+export default {
+	name: "Singer",
+	data() {
+		return {
+			value: "Vue"
+		};
+	},
+};
+</script>
+
+<style lang="css">
+	.singer__title {
+		padding: 20px;
+		color: rgb(80, 80, 80);
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import Singer from "./components/Singer.vue";
+
+/*
+	Mount point for javascript
+*/
+
+createApp(Singer).mount("#app");
