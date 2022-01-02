6070e81224b71f18a38d76542862ea53a0508f02
Rakotoarisoa Tahiriniaina Andrian <LenOkimaru@gmail.com>
2022-01-02 10:54:00 +0100

    minor changes before changing branche to implement the carousel

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,166 +1,171 @@
 <template>
-  <transition name="modal-fade">
-    <div class="modal-backdrop">
-      <div
-        class="modal"
-        role="dialog"
-        aria-labelledby="modalTitle"
-        aria-describedby="modalDescription"
-      >
-        <header class="modal-header" id="modalTitle">
-          <slot name="header"> This is the default title! </slot>
-          <button type="button" class="btn-close" @click="close">
-            <Icon icon="times" />
-          </button>
-        </header>
+	<transition name="modal-fade">
+		<div class="modal-backdrop">
+			<div
+				class="modal"
+				role="dialog"
+				aria-labelledby="modalTitle"
+				aria-describedby="modalDescription"
+			>
+				<header class="modal-header" id="modalTitle">
+					<slot name="header"> This is the default title! </slot>
+					<button type="button" class="btn-close" @click="close">
+						<Icon icon="times" />
+					</button>
+				</header>
 
-        <section class="modal-body" id="modalDescription">
-          <slot name="body">
-            <div class="gauche"></div>
+				<section class="modal-body" id="modalDescription">
+					<slot name="body">
+						<div class="content-left"></div>
 
-            <div class="droite">
-              <h2 class="title2">Description du produit :</h2>
-              <div class="separator3"></div></div
-          ></slot>
-        </section>
+						<div class="content-right">
+							<h2 class="title2">Description du produit :</h2>
+							<div class="separator3"></div></div
+					></slot>
+				</section>
 
-        <footer class="modal-footer">
-          <button
-            type="button"
-            class="btn-green"
-            @click="close"
-            aria-label="Close modal"
-          >
-            Fermer
-          </button>
-        </footer>
-      </div>
-    </div>
-  </transition>
+				<footer class="modal-footer">
+					<button
+						type="button"
+						class="btn-green"
+						@click="close"
+						aria-label="Close modal"
+					>
+						Fermer
+					</button>
+				</footer>
+			</div>
+		</div>
+	</transition>
 </template>
 
 <script>
 export default {
-  name: "Modal",
-  components: {},
-  methods: {
-    close() {
-      this.$emit("close");
-    },
-  },
+	name: "Modal",
+	components: {},
+	methods: {
+		close() {
+			this.$emit("close");
+		},
+	},
 };
 </script>
 
 <style lang="css">
 .modal-backdrop {
-  position: fixed;
-  top: 70px;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.3);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	position: fixed;
+	top: 70px;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background-color: rgba(0, 0, 0, 0.3);
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 
 .modal {
-  width: 80%;
-  height: 80vh;
-  background: #ffffff;
-  box-shadow: 2px 2px 20px 1px;
-  overflow-x: auto;
-  display: flex;
-  flex-direction: column;
+	width: 80%;
+	height: 80vh;
+	background: #ffffff;
+	border-radius: 12px;
+	box-shadow: 2px 2px 20px 1px;
+	overflow-x: auto;
+	display: flex;
+	flex-direction: column;
 }
 
 .modal-header,
 .modal-footer {
-  padding: 15px;
-  display: flex;
+	padding: 25px;
+	display: flex;
 }
 
 .modal-header {
-  background-color: #e30044;
-  font-size: 15pt;
-  display: inline-flex;
-  position: relative;
-  border-bottom: 1px solid #eeeeee;
-  color: #f7f7f7;
-  justify-content: center;
+	background-color: #e30044;
+	font-size: 15pt;
+	display: inline-flex;
+	position: relative;
+	border-bottom: 1px solid #eeeeee;
+	color: #f7f7f7;
+	justify-content: center;
 }
 
 .modal-body {
-  display: inline-flex;
-  column-gap: 15px;
-  padding: 20px 10px;
+	display: inline-flex;
+	column-gap: 15px;
+	padding: 20px 10px;
 }
 
 .separator3 {
-  width: 20%;
-  height: 5px;
-  background: #e30044;
+	width: 20%;
+	height: 5px;
+	background: #e30044;
 }
 
-.gauche {
-  box-shadow: 0px 0px 10px rgb(200, 200, 200);
-  width: 60%;
-  height: 55vh;
+.content-left {
+	padding: 10px 50px 10px 50px;
+
+	box-shadow: 0px 0px 10px rgb(200, 200, 200);
+	width: 60%;
+	height: 55vh;
 }
 
-.droite {
-  box-shadow: 0px 0px 10px rgb(200, 200, 200);
-  text-align: center;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  width: 40%;
-  height: 55vh;
+.content-right {
+	padding: 10px 50px 10px 50px;
+
+	box-shadow: 0px 0px 10px rgb(200, 200, 200);
+	text-align: center;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	width: 40%;
+	height: 55vh;
 }
 
 .btn-close {
-  position: absolute;
-  top: 0;
-  right: 0;
-  border: none;
-  font-size: 25px;
-  padding: 10px;
-  cursor: pointer;
-  font-weight: bold;
-  color: #ffffff;
-  background: transparent;
+	position: absolute;
+	top: 0;
+	right: 0;
+	border: none;
+	font-size: 25px;
+	padding: 10px;
+	cursor: pointer;
+	font-weight: bold;
+	color: #ffffff;
+	background: transparent;
 }
 
 .modal-footer {
-  margin-top: auto;
-  border-top: 1px solid #eeeeee;
-  align-items: baseline;
-  justify-content: center;
+	margin-top: auto;
+	border-top: 1px solid #eeeeee;
+	align-items: baseline;
+	justify-content: center;
 }
 
 .btn-green {
-  border: none;
-  color: white;
-  font-size: 12pt;
-  width: 100px;
-  padding: 10px;
-  background: #e30044;
-  border-radius: 10px;
-  cursor: pointer;
+	border: none;
+	color: white;
+	font-size: 12pt;
+	width: 100px;
+	padding: 10px;
+	background: #e30044;
+	border-radius: 10px;
+	cursor: pointer;
 }
 
 .modal-fade-enter,
 .modal-fade-leave-to {
-  opacity: 0;
+	opacity: 0;
 }
 
 .modal-fade-enter-active,
 .modal-fade-leave-active {
-  transition: opacity 0.5s ease-in-out;
+	transition: opacity 0.5s ease-in-out;
 }
 
 .title2 {
-  color: black;
-  font-size: 13pt;
+	color: black;
+	font-size: 13pt;
 }
 </style>
